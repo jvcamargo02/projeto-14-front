bff1a4804d3e49ad9d8088a13d6ca9ba3ee928d0
Mary Karakida <makarakida@gmail.com>
2022-07-11 13:54:45 -0300

    merge

--- src/components/PurchasePage.js
+++ src/components/PurchasePage.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Row, Card } from "react-bootstrap";
 
